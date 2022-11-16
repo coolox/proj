da30c27a1365d17ac0a55fa07c715f231d44e927
coolox <coolox@gmail.com>
2022-11-16 15:32:53 +0300

    add Homepage, Productpage & Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import { Routes, Route, Link } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Productpage from "./pages/Productpage";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/products" className="link">
+          Products
+        </Link>
       </header>
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/products" element={<Productpage />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+/* eslint-disable import/default */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+// eslint-disable-next-line import/namespace
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
